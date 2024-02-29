d940e6afe0a261cb6f3667d3ae0686506f766e31
KoalaPanda12 <katpla2004@gmail.com>
2024-02-28 17:54:52 -0800

    Adding Comments

--- EagleGame.py
+++ EagleGame.py
@@ -13,19 +13,26 @@ class Eagle:
         loss = speedlevel * (miles/10)
         return loss
     def calcualte_score(total_distance,energy,total_time):
+        '''Calculate the score of the player adding the total distance the eagle flew, 
+           the energy the eagle has and the total time the eagle spent flying '''
         final_score = (total_distance + energy) -total_time
         return final_score
     def calcuate_distance(x1,y1,x2,y2):
+        '''Calcuates the distance the eagle flew from the current 
+           point his at to the point the eagles wants to fly'''
         distance= math.sqrt((x2-x1)**2 + (y2-y1)**2)
         return round(distance)
     def calcuate_time(distance, speed):
+        '''Calcuates the time it took the eagles to fly to the next point'''
         time = distance/speed
         return time
     def calcualte_rest(energy,rest_hours):
+        '''Calcuates how much energy the eagles gets from resting'''
         energy = energy + rest_hours
         return energy
     def add_player(points,eaglename):
-        ''
+        '''Adds the palyers score to the text file and sorts them from 
+          highest score to lowest score'''
         with open("players.txt","r") as file:
             lines = file.readlines()
             player = f"{points}:{eaglename}\n"
@@ -47,11 +54,12 @@ add_energy =0
 num = 0
 xr= 0
 yr=0
+#Ask User to input an Eagle name
 eagle_name = input("Eagle Name: ")
 for i in range(1,6):
-    # Asking the user for name and 
     print(f"You are in round {i}/25")
     print(f"You have an energy level of {energy}")
+    #Asking the user if they want to fly or rest
     user_input = input("Would you like to rest or fly: ")
     user_input = user_input.lower()
     num = random.randint(0,1)
@@ -64,8 +72,10 @@ for i in range(1,6):
             yr = resource[2]
             add_energy = resource[0]
             print(f"There is a resource at({xr},{yr}). You will gain an energy level of {add_energy} ")
+        #User inputs where they want to fly 
         coordinate = input("Enter the coordiante you would like to fly to x,y: ")
         coordinate = coordinate.split(",")
+        #Users inputs what Speed they want to go 
         speed =input("Enter the speed you would like to go (1-10):")
         speed = int(speed)
         #Get the two coordinates 
